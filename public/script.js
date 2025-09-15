f6098b4e20986c9577ddaaa495f779b42887f666
yaikaew <41978321+yaikaew@users.noreply.github.com>
2025-09-15 10:05:43 +0700

    update script

--- public/script.js
+++ public/script.js
@@ -63,7 +63,7 @@ tabLinks2.forEach((link) => {
 
 // for overlay click
 document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.discography__card');
+    const cards = document.querySelectorAll('.ent-card');
 
     cards.forEach(card => {
         // Add click event listener for mobile devices
